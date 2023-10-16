fc070787e131508a6170f17c5ea4578abb73f905
Holly Tyler <hollystertyler@gmail.com>
2023-10-16 17:39:18 +0100

    deleted unecesarry files

--- src/main/java/com/codeclan/projectHere/components/DataLoader.java
+++ src/main/java/com/codeclan/projectHere/components/DataLoader.java
@@ -8,7 +8,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("!test") //Run every time EXCEPT Tests
+//@Profile("!test") //Run every time EXCEPT Tests
 //@Component
 public class DataLoader implements ApplicationRunner {
 
